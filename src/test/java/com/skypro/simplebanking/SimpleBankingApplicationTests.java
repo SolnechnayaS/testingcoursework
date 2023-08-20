ef1a26403d6d5b89d1bc377d8237188cfae0cd77
Svetlana Morozova <svetlanochka@yandex.ru>
2023-08-20 15:13:23 +0300

    Initial commit

--- src/test/java/com/skypro/simplebanking/SimpleBankingApplicationTests.java
+++ src/test/java/com/skypro/simplebanking/SimpleBankingApplicationTests.java
@@ -1,7 +1,19 @@
 package com.skypro.simplebanking;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.skypro.simplebanking.repository.AccountRepository;
+import com.skypro.simplebanking.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.junit.jupiter.Container;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SimpleBankingApplicationTests {
